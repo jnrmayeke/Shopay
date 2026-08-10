c68286544e1bc721567b27a2470da3e826d754a4
Jay <jnrmayeke@gmail.com>
2026-08-10 21:58:01 +0100

    updated inngest functions to recent

--- inngest/functions.js
+++ inngest/functions.js
@@ -2,8 +2,7 @@ import {inngest} from './client';
 
 // Inngest function to save user data to database
 export const syncUserCreation = inngest.createFunction(
-  {id: 'sync-user-create'},
-  {event: 'clerk/user.created'},
+  {id: 'sync-user-create', triggers: {event: 'clerk/user.created'}},
   async ({ event }) => {
     const {data} = event
     await prisma.user.create({
@@ -19,8 +18,7 @@ export const syncUserCreation = inngest.createFunction(
 
 // Inngest function to update user data to database
 export const syncUserUpdation = inngest.createFunction(
-  {id: 'sync-user-update'},
-  {event: 'clerk/user.updated'},
+  {id: 'sync-user-update', triggers: {event: 'clerk/user.updated'}},
   async ({ event }) => {
     const {data} = event
     await prisma.user.update({
@@ -36,8 +34,7 @@ export const syncUserUpdation = inngest.createFunction(
 
 // Inngest function to delete user data from database
 export const syncUserDeletion = inngest.createFunction(
-  {id: 'sync-user-delete'},
-  {event: 'clerk/user.deleted'},
+  {id: 'sync-user-delete', triggers: {event: 'clerk/user.deleted'}},
   async ({ event }) => {
     const {data} = event
     await prisma.user.delete({
